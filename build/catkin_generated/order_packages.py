09b71b4d8e4ddafb8c6587a03b2e08763e8e01ad
wifi-cable <wifi-cable@gmx.de>
2020-04-20 18:43:27 +0000

    removed devel and build folder, because commiting them was a really bad idea. I learned that today.

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/wifi-cable/Semester/Semester9/Robotik/catkin_ws/Robotic_AI_student_project/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
