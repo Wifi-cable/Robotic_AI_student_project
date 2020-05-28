3d8951cde70c0c5b591faedf4b05a02127aa7571
Steffen Matheis <steffen.matheis.97@gmail.com>
2020-05-28 13:21:09 +0200

    update ki scripts

--- keras/test_network.py
+++ keras/test_network.py
@@ -11,40 +11,101 @@
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
-import imutils
+from imutils import paths
 import cv2
+import os
+import sys
+
+# result vars
+tp = 0
+tpa = 0
+fp = 0
+fpa = 0
+tn = 0
+tna = 0
+fn = 0
+fna = 0
 
 
+# load the trained convolutional neural network
+print("[INFO] loading network...")
+model = load_model("marker_no_marker.model")
+
+imagePaths = sorted(list(paths.list_images("images/")))
+for imagePath in imagePaths:
+    # load image, preprocess it
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (160, 120))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    # classify this image, print result
+    label = imagePath.split(os.path.sep)[-2] == "marker"
+    (nm, m) = model.predict(image)[0]
+    probability = max(nm, m)
+    has_marker = m > nm
+
+    if label:
+        if has_marker:
+            tp += 1
+            tpa += probability
+            result = "true positive"
+        else:
+            fn += 1
+            fna += probability
+            result = "false negative"
+    else:
+        if has_marker:
+            fp += 1
+            fpa += probability
+            result = "false positive"
+        else:
+            tn += 1
+            tna += probability
+            result = "true negative"
+
+    print("{}: {} with {:.2f} probability".format(
+        imagePath, result, probability))
+
+print("##########RESULTS##########")
+tpa = tpa / tp
+fpa = fpa / fp
+tna = tna / tn
+fna = fna / fn
+print("{} true positives,  {}% probability".format(tp, tpa))
+print("{} false positives, {}% probability".format(fp, fpa))
+print("{} true negatives,  {}% probability".format(tn, tna))
+print("{} false negatives, {}% probability".format(fn, fna))
+
+""" 
 
 # load the image
-image = cv2.imread("toy2.jpg")
+image = cv2.imread(sys.argv[1])
 orig = image.copy()
 
 # pre-process the image for classification
-image = cv2.resize(image, (128, 128))
+image = cv2.resize(image, (320, 240))
 image = image.astype("float") / 255.0
 image = img_to_array(image)
 image = np.expand_dims(image, axis=0)
 
-# load the trained convolutional neural network
-print("[INFO] loading network...")
-model = load_model("toy_not_toy.model")
 
 # classify the input image
-(nottoy, toy) = model.predict(image)[0]
-print("toy = ",toy, " Not Toy = ",nottoy)
+(nomarker, marker) = model.predict(image)[0]
+print("Marker = ", marker, " No Marker = ", nomarker)
 # build the label
-label = "toy" if toy > nottoy else "Not toy"
-probability = max(toy, nottoy)
+label = "Marker" if marker > nomarker else "No Marker"
+probability = max(marker, nomarker)
 label = "{}: {:.2f}%".format(label, probability * 100)
 
 # draw the label on the image
 output = imutils.resize(orig, width=400)
 cv2.putText(output, label, (200, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (200, 255, 40), 2)
+			0.7, (200, 255, 40), 2)
 
 # show the output image
 cv2.imshow("Output", output)
-cv2.imwrite("toy_classify2.jpg", output)
+cv2.imwrite("marker_classify2.jpg", output)
 cv2.waitKey(0)
-
+ """
--- keras/trainTheCNN.py
+++ keras/trainTheCNN.py
@@ -1,10 +1,9 @@
 # program to train Convolution Neural Network to detect toys and not toys
 
 
-
 # import the necessary packages
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam, SGD,Adadelta,Adagrad
+from keras.optimizers import Adam, SGD, Adadelta, Adagrad
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
 from keras.utils import to_categorical
@@ -19,58 +18,59 @@ from imutils import paths
 import numpy as np
 import random
 import cv2
-import os, time
-
+import os
+import time
 
 
 class LeNet:
-	@staticmethod
-	def build(width, height, depth, classes):
-		# initialize the model
-		model = Sequential()
-		inputShape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model
+        model = Sequential()
+        inputShape = (height, width, depth)
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
 
-		# First layer is a convolution with 20 functions and a kernel size of 5x5 (2 neighbor pixels on each side)
-		model.add(Conv2D(20, (5, 5), padding="same",
-			input_shape=inputShape))
-		# our activation function is ReLU (Rectifier Linear Units)
-		model.add(Activation("relu"))
-		# second layer is maxpooling 2x2 that reduces our image resolution by half 
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # First layer is a convolution with 20 functions and a kernel size of 5x5 (2 neighbor pixels on each side)
+        model.add(Conv2D(20, (5, 5), padding="same",
+                         input_shape=inputShape))
+        # our activation function is ReLU (Rectifier Linear Units)
+        model.add(Activation("relu"))
+        # second layer is maxpooling 2x2 that reduces our image resolution by half
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-		# Third Layer - Convolution, twice the size of the first convoltion
-		model.add(Conv2D(40, (5, 5), padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # Third Layer - Convolution, twice the size of the first convoltion
+        model.add(Conv2D(40, (5, 5), padding="same"))
+        model.add(Activation("relu"))
 
-		# Fifth Layer is Full connected flattened layer that makes our 3D images into 1D arrays
-		model.add(Flatten())
-		model.add(Dense(500))
-		model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # Fifth Layer is Full connected flattened layer that makes our 3D images into 1D arrays
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
 
-		# return the completed network architecture
-		return model
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-
-
+        # return the completed network architecture
+        return model
 
 
 # data files
+# choose image directory
 imageDirectory = "images/"
-model_file = "toy_not_toy.model"
+model_file = "marker_no_marker.model"
 
 # Set up the number of training passes (epochs), learning rate, and batch size
-EPOCHS = 100
+EPOCHS = 10
 LEARNING_RATE = 1e-4
-BATCH_SIZE = 32
+BATCH_SIZE = 8
+IMG_WIDTH = 160
+IMG_HEIGHT = 120
 
 # initialize the data and labels
 print("Loading training images set...")
@@ -85,33 +85,38 @@ random.seed(seed)
 random.shuffle(imagePaths)
 print("number of images", len(imagePaths))
 print(imagePaths[0])
-
+m = 0
+nm = 0
 # loop over the input images
 for imagePath in imagePaths:
-	# load the image, pre-process it, and store it in the data list
-	image = cv2.imread(imagePath)
-	image = cv2.resize(image, (128, 128))
-	image = img_to_array(image)
-	data.append(image)
+    # load the image, pre-process it, and store it in the data list
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT))
+    image = img_to_array(image)
+    data.append(image)
 
-	# extract the class label from the image path and update the
-	# labels list
-	label = imagePath.split(os.path.sep)[-2]
-	print(label)
-	if label == "images/toys":
-		label = 1
-		print("Make Me a Toy")
-	else: 
-		label = 0
-	labels.append(label)
+    # extract the class label from the image path and update the
+    # labels list
+    label = imagePath.split(os.path.sep)[-2]
+
+    if label == "marker":
+        label = 1
+        m += 1
+    else:
+        label = 0
+        nm += 1
+    labels.append(label)
 
 # Normalize the values of the pixels to be between 0 and 1 instead of 0 to 255
 data = np.array(data, dtype="float") / 255.0
 labels = np.array(labels)
 
+print(labels)
+print("#markers", m, "#no markers", nm)
+
 # split the data between a testing set and a training set
 (trainX, testX, trainY, testY) = train_test_split(data,
-	labels, test_size=0.20, random_state=seed)
+                                                  labels, test_size=0.20, random_state=seed)
 
 # convert the labels from integers to vectors
 trainY = to_categorical(trainY, num_classes=2)
@@ -119,30 +124,29 @@ testY = to_categorical(testY, num_classes=2)
 
 # construct the image generator for data augmentation
 aug = ImageDataGenerator(rotation_range=40, width_shift_range=0.2,
-	height_shift_range=0.2, shear_range=0.1, zoom_range=0.2,
-	horizontal_flip=True, fill_mode="nearest")
+                         height_shift_range=0.2, shear_range=0.1, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
 
 # initialize the weights of the network
 print("compiling CNN network...")
-cnNetwork = LeNet.build(width=128, height=128, depth=3, classes=2)
-#use adam optimizer.  tried SGD for comparison - needs more epochs (>100)
+cnNetwork = LeNet.build(width=IMG_WIDTH, height=IMG_HEIGHT, depth=3, classes=2)
+# use adam optimizer.  tried SGD for comparison - needs more epochs (>100)
 opt = Adam(lr=LEARNING_RATE, decay=LEARNING_RATE / EPOCHS)
 #opt = SGD(lr=LEARNING_RATE,decay=LEARNING_RATE / EPOCHS)
 
 # if we have more than two classes, use categorical crossentropy
 cnNetwork.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+                  metrics=["accuracy"])
 
 # train the network - here is where the magic happens...
 print("training network...")
-print("length trainx", len(trainX)," length trainy ",len(trainY))
+print("length trainx", len(trainX), " length trainy ", len(trainY))
 
 H = cnNetwork.fit_generator(aug.flow(trainX, trainY, batch_size=BATCH_SIZE),
-	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BATCH_SIZE,
-	epochs=EPOCHS, verbose=1)
+                            validation_data=(testX, testY), steps_per_epoch=len(
+                                trainX) // BATCH_SIZE,
+                            epochs=EPOCHS, verbose=1)
 
 # save the CNN network weights to file
 print("Saving Network Weights to file...")
 cnNetwork.save(model_file)
-
-
