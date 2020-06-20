ca05d5f320eda43602dada0d9fecc99eefa1bd66
Steffen Matheis <steffen.matheis.97@gmail.com>
2020-06-20 15:47:53 +0200

    try to predict where marker is in image
    accidentally formatted snap_data_img.py, sorry :/

--- src/find_marker.py
+++ src/find_marker.py
@@ -1,25 +1,110 @@
 #!/usr/bin/env python3
-import cv2	#opencv
-import sys	#fileIO stuff
-import os		#other fileIO stuff
-import numpy	#for opencv
-import rospy	#need Ros Python
-from std_msgs.msg import String # ROS python needs a Ros message to publish
+from cv2 import cv2  # opencv
+import enum
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+import numpy  # for opencv
+import os  # other fileIO stuff
+import rospy  # need Ros Python
+from std_msgs.msg import String  # ROS python needs a Ros message to publish
+import sys  # fileIO stuff
 
+from snap_data_img import snapPicture
 '''
 TODO
-- take picture
-- use opencv to load picture in variable
-- cut picture in 9 pices
-- deploy model of Neural Net
-- seach for markler in pices (or in whole picture first)
-- figure out if marker is in left, right, center or nix. 
-- publish result as string
+- take picture ✔
+- use opencv to load picture in variable ✔
+- cut picture in 9 pices ✔
+- deploy model of Neural Net ❓
+- seach for markler in pices (or in whole picture first 𝙭) ✔
+- figure out if marker is in left, right, center or nix. ✔
+- publish result as string ✔
 
 '''
-pub = rospy.Publisher('Marker', String, queue_size=10)	#instead of return you publish
-rospy.init_node('find_marker')	# naming a node makes a lot of sense for debugging. choose any name.
-myRate = rospy.Rate(10) # 10hz	#10 may need adjustment 
-while not rospy.is_shutdown():	#not while true,  it is while not ctrl+C from terminal.
-   pub.publish("hello world")	#replace by String message to tell us where the marker is
-   myRate.sleep()		#like delay. to not spam my terminal 100 times per second 
+
+# change these as needed
+
+
+class Move(enum.Enum):
+    Left: "left"
+    Right: "right"
+    Forward: "forward"
+    NotFound: "notfound"
+
+
+IMG_WIDTH = 160
+IMG_HEIGHT = 160
+
+model = load_model(
+    'PATH_TO_MODEL I don\'t know where it is please someone do it for me')
+
+# instead of return you publish
+pub = rospy.Publisher('Marker', String, queue_size=10)
+# naming a node makes a lot of sense for debugging. choose any name.
+rospy.init_node('find_marker')
+myRate = rospy.Rate(10)  # 10hz	#10 may need adjustment
+
+# not while true,  it is while not ctrl+C from terminal.
+while not rospy.is_shutdown():
+    capture = snapPicture(False, False, False)  # snap image, save to capture
+    # ! unsure if this works, may need to use cv2.imread(capture)
+    image = cv2.resize(capture, (IMG_WIDTH, IMG_HEIGHT))
+
+    tiles = []
+    results = []
+
+    # split into 9 tiles
+    step_width = IMG_WIDTH // 3
+    step_height = IMG_HEIGHT // 3
+    for i in range(3):
+        for j in range(3):
+            x_start, x_end = i * step_width, (i + 1) * step_width
+            y_start, y_end = j * step_height, (j + 1) * step_height
+
+            tiles.append(cv2.resize(
+                image[x_start:x_end, y_start:y_end], (IMG_WIDTH, IMG_HEIGHT)))
+            print("{}:{}x{}:{}".format(x_start, x_end, y_start, y_end))
+
+    # predict every tile and append to result
+    for tile in tiles:
+        tile = numpy.expand_dims(img_to_array(
+            tile.astype("float") / 255.0), axis=0)
+
+        (nm, m) = model.predict(tile)[0]
+        probability = max(nm, m) * 100
+        has_marker = m > nm
+
+        results.append([has_marker, probability])
+
+    # sort results into columns & calculate average
+    # super weird code but works haha
+    columns = []
+    for i in range(3):
+        column = [0, 0.0]  # [number of markers in column, average probability]
+        for j in range(3):
+            if results[i + j * 3][0] == True:
+                column[0] += 1
+                column[1] += results[i + j * 3][1]
+        if column[0] > 0:
+            # calculate average probability
+            column[1] = column[1] / column[0]
+        columns.append(column)
+
+    # publish where to go
+    # max(columns) does sort by num of markers first, avg. probability second
+    if columns[0] == max(columns) and columns[0] == [0, 0.0]:
+        print("No marker detected anywhere.")
+        pub.publish(Move.NotFound)
+    elif columns[0] == max(columns):
+        print("Go left, I'm {}% sure".format(columns[0][1]))
+        pub.publish(Move.Left)
+    elif columns[1] == max(columns):
+        print("Go forward, I'm {}% sure".format(columns[1][1]))
+        pub.publish(Move.Forward)
+    elif columns[2] == max(columns):
+        print("Go right, I'm {}% sure".format(columns[2][1]))
+        pub.publish(Move.Right)
+    else:
+        print("Should not happen.")
+
+    myRate.sleep()  # like delay. to not spam my terminal 100 times per second
--- src/snap_data_img.py
+++ src/snap_data_img.py
@@ -6,84 +6,97 @@ import argparse
 import os
 import numpy
 
+
 def undistort(imgName):
-	img = imgName
+    img = imgName
 
-	#need mtx and dist
-	mtx = numpy.array([[1.520925808570831805e+03,0.000000000000000000e+00,7.713748717157859573e+02],
-	[0.000000000000000000e+00,1.519652028136312083e+03,5.851593992111154421e+02],
-	[0.000000000000000000e+00,0.000000000000000000e+00,1.000000000000000000e+00]])
-	dist =numpy.array([1.685848346581539092e-01,-5.558901276684754533e-01,-4.224284338330285687e-03,-2.539218024257525446e-03,5.383658295121591664e-01])
-	h,  w = img.shape[:2]
-	newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-	
-	# undistort
-	dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-	
-	x,y,w,h = roi	#cropping
-	dst = dst[y:y+h, x:x+w]
-	#cv2.imwrite('calibresult.png',dst)
-	return dst
+    # need mtx and dist
+    mtx = numpy.array([[1.520925808570831805e+03, 0.000000000000000000e+00, 7.713748717157859573e+02],
+                       [0.000000000000000000e+00, 1.519652028136312083e+03,
+                           5.851593992111154421e+02],
+                       [0.000000000000000000e+00, 0.000000000000000000e+00, 1.000000000000000000e+00]])
+    dist = numpy.array([1.685848346581539092e-01, -5.558901276684754533e-01, -
+                        4.224284338330285687e-03, -2.539218024257525446e-03, 5.383658295121591664e-01])
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+        mtx, dist, (w, h), 1, (w, h))
 
-def snapPicture(marker=False, show=False):
-	print("paramcheck marker="+ str(marker)+ " show = "+str(show))
-	dayTime = datetime.datetime.now()	#get unix time of when picture was taken
-	now = dayTime.strftime("%s")	#save as string
-	cap = cv2.VideoCapture(0)
-	ret, capture = cap.read()
-	pictureName = "data" + now +".png"
-	print(pictureName)
-#if immage captured correctly save it.
-	print("taking a picture was sucessfull? " + str(ret))
-	#undistort
-	#capture = undistort(capture)
-	if ret:	
-		if marker:     #save in folder with marker
-			folder = "marker/"
-			cv2.imwrite((folder + pictureName), capture)
-		else:    #save in other folder
-			folder = "no_marker/"
-			cv2.imwrite((folder + pictureName), capture)
-		print("saved a picture named "+ pictureName +" to the folder " + folder) 
-		
-		#print("should i show the immage? "+ str(show))
-		if show: 
-			cv2.imshow(pictureName,capture)
-			cv2.waitKey(1000) & 0xFF
-			cv2.destroyAllWindows()
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    x, y, w, h = roi  # cropping
+    dst = dst[y:y+h, x:x+w]
+    # cv2.imwrite('calibresult.png',dst)
+    return dst
+
+
+def snapPicture(marker=False, show=False, saveToFile=True):
+    print("paramcheck marker=" + str(marker) + " show = "+str(show))
+    dayTime = datetime.datetime.now()  # get unix time of when picture was taken
+    now = dayTime.strftime("%s")  # save as string
+    cap = cv2.VideoCapture(0)
+    ret, capture = cap.read()
+    pictureName = "data" + now + ".png"
+    print(pictureName)
+# if immage captured correctly save it.
+    print("taking a picture was sucessfull? " + str(ret))
+    # undistort
+    #capture = undistort(capture)
+    if saveToFile:
+        if ret:
+            if marker:  # save in folder with marker
+                folder = "marker/"
+                cv2.imwrite((folder + pictureName), capture)
+            else:  # save in other folder
+                folder = "no_marker/"
+                cv2.imwrite((folder + pictureName), capture)
+            print("saved a picture named " +
+                  pictureName + " to the folder " + folder)
+
+            #print("should i show the immage? "+ str(show))
+            if show:
+                cv2.imshow(pictureName, capture)
+                cv2.waitKey(1000) & 0xFF
+                cv2.destroyAllWindows()
+    else:
+        return capture
 
 
 def main():
-	# ---- global variables with  DEFAULT VALUES ---
-	marker = False
-	show = False
-	
-	parser = argparse.ArgumentParser( description="Saves snapshot from the camera.")
-	parser.add_argument("--show", default=False, type=bool, help="do you want to display each picture you take?")
-	parser.add_argument("--marker", default=False, type=bool, help="did you take a picture of the marker? True/ False")
-	
-	args = parser.parse_args()
-	marker = args.marker
-	show = args.show
+    # ---- global variables with  DEFAULT VALUES ---
+    marker = False
+    show = False
 
-	print(str(marker))
-	#print(str(show))
+    parser = argparse.ArgumentParser(
+        description="Saves snapshot from the camera.")
+    parser.add_argument("--show", default=False, type=bool,
+                        help="do you want to display each picture you take?")
+    parser.add_argument("--marker", default=False, type=bool,
+                        help="did you take a picture of the marker? True/ False")
 
-	if not os.path.exists("marker"):
-		print("got no directory 'marker', will make one for you now")
-		try:
-			os.makedirs("marker")
-		except:
-			print("can not create a directory like that")
+    args = parser.parse_args()
+    marker = args.marker
+    show = args.show
 
-	if not os.path.exists("no_marker"):
-		print("did not find directory 'no_maker', will build if for you now")
-		try:
-			os.makedirs("no_marker")
-		except:
-			print("can not create a directory like that")
-	snapPicture(marker, show)
+    print(str(marker))
+    # print(str(show))
+
+    if not os.path.exists("marker"):
+        print("got no directory 'marker', will make one for you now")
+        try:
+            os.makedirs("marker")
+        except:
+            print("can not create a directory like that")
+
+    if not os.path.exists("no_marker"):
+        print("did not find directory 'no_maker', will build if for you now")
+        try:
+            os.makedirs("no_marker")
+        except:
+            print("can not create a directory like that")
+    snapPicture(marker, show, True)
+
 
 if __name__ == "__main__":
-	main()
-#not main no more
+    main()
+# not main no more
